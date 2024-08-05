015dadc90e57235dcd40cb9b68ae5ec6a1e0fceb
kidum-netizen <frankkibet96@gmail.com>
2024-08-05 05:56:57 -0700

    restful apiu

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
-"""app.py to connect to API"""
-import os
-from models import storage
+"""
+Module app
+"""
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
-from flask_cors import CORS
+from flasgger import Swagger
+from flask import (Blueprint, Flask, jsonify, make_response)
+from flask_cors import (CORS, cross_origin)
+from models import storage
+from os import getenv
 
 
 app = Flask(__name__)
+CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
-
-
-@app.teardown_appcontext
-def teardown_appcontext(code):
-    """teardown_appcontext"""
-    storage.close()
+Swagger(app)
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def not_found(error):
+    """json 404 page"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ closes the session """
+    storage.close()
+
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')))
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+#    print(app.url_map)
+    app.run(host=host, port=port)
